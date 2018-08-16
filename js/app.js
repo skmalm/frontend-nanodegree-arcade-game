54e5fa1aab5e66e1cdde271a77e2176d03079d63
Sebastian-Malm <skmalm@gmail.com>
2018-08-16 17:49:34 -0400

    refactor: incorporate Enemy render method into class instead of separate prototype

--- js/app.js
+++ js/app.js
@@ -17,11 +17,10 @@ class Enemy {
       // which will ensure the game runs at the same speed for
       // all computers.
     }
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    // Draw the enemy on the screen, required method for game
+    render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 };
 
 // Now write your own player class
